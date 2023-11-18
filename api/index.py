9939108041b29d71c5f6cd40762aa59b7d69af57
Junaid <66956290+MhJunaid1@users.noreply.github.com>
2023-11-18 23:40:15 +0500

    changed to the twilio flask app

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
@@ -9,3 +10,15 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    """Respond to incoming calls with a simple text message."""
+    # Fetch the message
+    msg = request.form.get('Body')
+
+    # Create reply
+    resp = MessagingResponse()
+    resp.message("You said: {}".format(msg))
+
+    return str(resp)
