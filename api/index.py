a7f93bed54977ad4f5f8e74de82dccff406768a8
Junaid <66956290+MhJunaid1@users.noreply.github.com>
2023-11-19 00:20:11 +0500

    after adding get all products api's in the flask app

--- api/index.py
+++ api/index.py
@@ -1,7 +1,18 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import requests
 
 app = Flask(__name__)
+
+# Function to fetch product data from the API
+def get_product_data():
+    # Make a GET request to the API endpoint that provides product data
+    response = requests.get('https://inventory-website.vercel.app/api/product/getPs')
+    
+    if response.status_code == 200:
+        return response.json().get('allProducts')  # Extracting product data from the response
+    else:
+        return None
 
 @app.route("/")
 def hello():
@@ -9,13 +20,23 @@ def hello():
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
-    """Respond to incoming calls with a simple text message."""
     # Fetch the message
     msg = request.form.get('Body')
 
+    # Get product data
+    products = get_product_data()
+
     # Create reply
     resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
+    
+    if products:
+        # Format product data as a string
+        product_list = "\n\n".join([f"Name: {product['name']}\nDescription: {product['description']}\nPrice: {product['price']}" for product in products])
+
+        # Send product data in the SMS response
+        resp.message(f"Products:\n{product_list}")
+    else:
+        resp.message("Failed to fetch product data")
 
     return str(resp)
 
