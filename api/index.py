98c410b6769cdf37ff24ed936c1485c95ca71eae
L181119umarShahzad <47095966+L181119umarShahzad@users.noreply.github.com>
2023-11-29 19:36:13 +0500

    menu driven logic incorporated

--- api/index.py
+++ api/index.py
@@ -1,106 +1,36 @@
-from flask import Flask, request
+from flask import Flask, request,session
+import requests
 from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecretkeyjuni'
 
-class InventoryManager:
-    def __init__(self):
-        self.current_menu = "main"
-        self.products = []
-        self.suppliers = []
-        self.employees = []
+def get_products():
+    # Make a GET request to the API endpoint that provides product data
+    response = requests.get('https://inventory-website.vercel.app/api/product/getPs')
 
-    def handle_main_menu(self, option):
-        reply = ""
-        if option == '1':
-            reply = "1. Add a product\n2. Remove a product\n3. Edit a product\n4. Show all the products\n5. Return to the main menu"
-            self.current_menu = "product_menu"
-        elif option == '2':
-            reply = "1. Add a supplier\n2. Remove a supplier\n3. Edit a supplier\n4. Show all the suppliers\n5. Return to the main menu"
-            self.current_menu = "supplier_menu"
-        elif option == '3':
-            reply = "1. Add an employee\n2. Remove an employee\n3. Edit an employee\n4. Show all the employees\n5. Return to the main menu"
-            self.current_menu = "employee_menu"
-        elif option == '4':
-            reply = "General information about the whole system"
-            # AI chatbot interaction or general information
-        else:
-            reply = "Invalid option. Please choose a valid option."
+    if response.status_code == 200:
+        return response.json().get('allProducts')  # Extracting product data from the response
+    else:
+        return None
 
-        return reply
+def get_suppliers():
+    # Make a GET request to the API endpoint that provides supplier data
+    response = requests.get('https://inventory-website.vercel.app/api/supplier/getSs')
 
+    if response.status_code == 200:
+        return response.json().get('allSuppliers')  # Extracting product data from the response
+    else:
+        return None  
 
-    def handle_product_menu(self, option):
-        reply = ""
-        if option == '1':
-            reply = "Please provide details of the product in the format:\nname,description,price,quantity,unitOfMeasure,category,brand,sku"
-            # Handle adding a product
-        elif option == '2':
-            reply = "Please provide the name of the product you want to delete"
-            # Handle removing a product
-        elif option == '3':
-            reply = "Please provide the name of the product you want to edit"
-            # Handle editing a product
-        elif option == '4':
-            reply = "List of Products:\n"
-            for product in self.products:
-                reply += f"{product['name']} - {product['price']}\n"
-        elif option == '5':
-            reply = "Returning to the main menu"
-            self.current_menu = "main"
-        else:
-            reply = "Invalid option. Please choose a valid option."
+def get_employees():
+    # Make a GET request to the API endpoint that provides employee data
+    response = requests.get('https://inventory-website.vercel.app/api/employee/getEs')
 
-        return reply
-
-    def handle_supplier_menu(self, option):
-        reply = ""
-        if option == '1':
-            reply = "Please provide details of the supplier in the format:\nname,contactPerson,email,phone,address"
-            # Handle adding a supplier
-        elif option == '2':
-            reply = "Please provide the name of the supplier you want to delete"
-            # Handle removing a supplier
-        elif option == '3':
-            reply = "Please provide the name of the supplier you want to edit"
-            # Handle editing a supplier
-        elif option == '4':
-            reply = "List of Suppliers:\n"
-            for supplier in self.suppliers:
-                reply += f"{supplier['name']} - {supplier['contactPerson']}\n"
-        elif option == '5':
-            reply = "Returning to the main menu"
-            self.current_menu = "main"
-        else:
-            reply = "Invalid option. Please choose a valid option."
-
-        return reply
-
-    def handle_employee_menu(self, option):
-        reply = ""
-        if option == '1':
-            reply = "Please provide details of the employee in the format:\nname,email,phone,address,position,hireDate,salary,workingHours,status"
-            # Handle adding an employee
-        elif option == '2':
-            reply = "Please provide the name of the employee you want to delete"
-            # Handle removing an employee
-        elif option == '3':
-            reply = "Please provide the name of the employee you want to edit"
-            # Handle editing an employee
-        elif option == '4':
-            reply = "List of Employees:\n"
-            for employee in self.employees:
-                reply += f"{employee['name']} - {employee['position']}\n"
-        elif option == '5':
-            reply = "Returning to the main menu"
-            self.current_menu = "main"
-        else:
-            reply = "Invalid option. Please choose a valid option."
-
-        return reply
-
-
-inventory_manager = InventoryManager()
+    if response.status_code == 200:
+        return response.json().get('allEmployees')  # Extracting product data from the response
+    else:
+        return None      
 
 @app.route("/")
 def hello():
@@ -109,24 +39,165 @@ def hello():
 @app.route("/sms", methods=['POST'])
 def sms_reply():
     msg = request.form.get('Body').lower()
-    phone_no = request.form.get('From')
-    if msg=="1" or msg=="2" or msg=="3":
-        inventory_manager.current_menu=msg
+    user_phone = request.form.get('From')
+    user_session = session.get(user_phone, {'first_time': True})
+
+    msg = request.form.get('Body').lower()
     resp = MessagingResponse()
 
-    reply = ""
-    if inventory_manager.current_menu == "main":
+    if user_session['first_time']:
         reply = "Welcome to the Inventory Management Website\n1. Information regarding Products\n2. Information regarding Suppliers\n3. Information regarding Employees\n4. General information about the whole system"
         resp.message(reply)
-    elif inventory_manager.current_menu == "1":
-        reply = inventory_manager.handle_main_menu(msg)
-        resp.message(reply)
-    elif inventory_manager.current_menu == "2":
-        reply = inventory_manager.handle_main_menu(msg)
-        resp.message(reply)
-    elif inventory_manager.current_menu == "3":
-        reply = inventory_manager.handle_main_menu(msg)
-        resp.message(reply)
+        user_session['first_time'] = False
+        session[user_phone] = user_session
+    else:
+        first_menu = user_session.get('first_menu')
+        second_menu = user_session.get('second_menu')
+        
+        if not first_menu:
+            if msg == '1':
+                user_session['first_menu'] = 'productmenu'
+                first_menu = 'productmenu'
+                reply = "1. Add a product\n2. Remove a product\n3. Edit a product\n4. Show all the products\n5. Return to the main menu"
+
+            elif msg == '2':
+                user_session['first_menu'] = 'suppliermenu'
+                first_menu = 'suppliermenu'
+                reply = "1. Add a supplier\n2. Remove a supplier\n3. Edit a supplier\n4. Show all the suppliers\n5. Return to the main menu"
+
+            elif msg == '3':
+                user_session['first_menu'] = 'employeemenu'
+                first_menu = 'employeemenu'
+                reply = "1. Add an employee\n2. Remove an employee\n3. Edit an employee\n4. Show all the employees\n5. Return to the main menu"
+
+            else:
+                reply = "Invalid option selected"
+    
+            resp.message(reply)
+            return str(resp)
+
+        if first_menu == 'productmenu':
+            if not second_menu:
+                # Handle product menu options
+                if msg == '1':
+                    user_session['second_menu'] = 'addproduct'
+                    second_menu = 'addproduct'
+                    reply = "Please provide details of the product in the format:\nname,description,price,quantity,unitOfMeasure,category,brand,sku"
+                elif msg == '2':
+                    user_session['second_menu'] = 'removeproduct'
+                    second_menu = 'removeproduct'
+                    reply = "Please provide the name of the product you want to delete"
+                    # Handle removing a product
+                elif msg == '3':
+                    user_session['second_menu'] = 'editproduct'
+                    second_menu = 'editproduct'
+                    reply = "Please provide the name of the product you want to edit"
+                    # Handle editing a product
+                elif msg == '4':
+                    reply = "List of Products:\n"
+                    # Get product data
+                    products = get_products()
+                    if products:
+                        # Format product data as a string
+                        product_list = "\n\n".join([f"Name: {product['name']}\nDescription: {product['description']}\nPrice: {product['price']}" for product in products])
+                        resp.message(f"Products:\n{product_list}")
+                    else:
+                        resp.message("Failed to fetch product data")
+                    
+                    return str(resp)
+
+                    #call the api to get all the products
+                elif msg == '5':
+                    reply = "Returning to the main menu\n1. Information regarding Products\n2. Information regarding Suppliers\n3. Information regarding Employees\n4. General information about the whole system"
+                    #delete all the first_menu, secon_menu and first_Time if necessary
+                else:
+                    reply = "Invalid option. Please choose a valid option."
+                resp.message(reply)
+                return str(resp)
+
+                # ... handle other options for product menu
+
+        elif first_menu == 'suppliermenu':
+            if not second_menu:
+                # Handle supplier menu options
+                if msg == '1':
+                    user_session['second_menu'] = 'addsupplier'
+                    second_menu = 'addsupplier'
+                    reply = "Please provide details of the supplier in the format:\nname,contactPerson,email,phone,address"
+                elif msg == '2':
+                    user_session['second_menu'] = 'removesupplier'
+                    second_menu = 'removesupplier'
+                    reply = "Please provide the name of the supplier you want to delete"
+                    # Handle removing a supplier
+                elif msg == '3':
+                    user_session['second_menu'] = 'editsupplier'
+                    second_menu = 'editsupplier'
+                    reply = "Please provide the name of the supplier you want to edit"
+                    # Handle editing a supplier
+                elif msg == '4':
+                    reply = "List of Suppliers:\n"
+                    
+                    # Get supplier data
+                    products = get_suppliers()
+                    if products:
+                        # Format product data as a string
+                        product_list = "\n\n".join([f"Name: {product['name']}\nDescription: {product['description']}\nPrice: {product['price']}" for product in products])
+                        resp.message(f"Suppliers are:\n{product_list}")
+                    else:
+                        resp.message("Failed to fetch product data")
+                    
+                    return str(resp)
+                    #logic for getting the list of suppliers
+                elif msg == '5':
+                    reply = "Returning to the main menu\n1. Information regarding Products\n2. Information regarding Suppliers\n3. Information regarding Employees\n4. General information about the whole system"
+                    #logic for going back
+                else:
+                    reply = "Invalid option. Please choose a valid option."
+
+                resp.message(reply)
+                return str(resp)
+
+        elif first_menu == 'employeemenu':
+            if not second_menu:
+                
+                if msg == '1':
+                    user_session['second_menu'] = 'addemployee'
+                    second_menu = 'addemployee'
+                    reply = "Please provide details of the employee in the format:\nname,email,phone,address,position,hireDate,salary,workingHours,status"
+
+                elif msg == '2':
+                    user_session['second_menu'] = 'removeemployee'
+                    second_menu = 'removeemployee'
+                    reply = "Please provide the name of the employee you want to delete"
+            
+                elif msg == '3':
+                    user_session['second_menu'] = 'editemployee'
+                    second_menu = 'editemployee'
+                    reply = "Please provide the name of the employee you want to edit"
+                    # Handle editing an employee
+                elif msg == '4':
+                    reply = "List of Employees:\n"
+                    
+                elif msg == '5':
+                    reply = "Returning to the main menu\n1. Information regarding Products\n2. Information regarding Suppliers\n3. Information regarding Employees\n4. General information about the whole system"
+
+                else:
+                    reply = "Invalid option. Please choose a valid option."
+                resp.message(reply)
+                return str(resp)    
+
+
+        session[user_phone] = user_session
+
+        # Respond based on the menus
+        if second_menu:
+            # Handle responses based on second_menu
+            pass
+        else:
+            # Respond based on first_menu
+            pass
+
+   
 
     return str(resp)
 
